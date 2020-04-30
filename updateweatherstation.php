bbbf9cd8f8b9ee8ea11075455fb65d4ba11d9a17
buffy007 <55991786+buffy007@users.noreply.github.com>
2020-04-30 11:38:36 +0200

    Add files via upload

---
+++ updateweatherstation.php
@@ -0,0 +1,11 @@
+<?php
+$export = "{";
+foreach ($_GET as $key => $value) {
+	$export .=  '"'.$key . '":"' . $value . '",';
+}
+$export .= '"manufacturer":"Garni"}';
+echo $export;
+$file = fopen("data/garni.json", "w+");        
+fwrite($file, $export);
+fclose($file);
+?>

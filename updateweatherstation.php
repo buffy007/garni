a2a27013a8ad212ddca295e427d5f93000f31ae9
buffy.cz <55991786+buffy007@users.noreply.github.com>
2020-04-30 14:19:38 +0200

    Update updateweatherstation.php

--- updateweatherstation.php
+++ updateweatherstation.php
@@ -1,13 +1,13 @@
 <?php
 // script: Garni Weather Station server PHP script
-// version: 1.0
+// version: 1.0 (MIT licence)
 // author: Petr Fismol (buffy.cz)
 $export = "{";
 foreach ($_GET as $key => $value) {
 	$export .=  '"' . $key . '":"' . $value . '",';
 }
 $export .= '"manufacturer":"Garni"}';
-echo $export;
+echo $export; // comment this line for switch off browser data output
 $file = fopen("data/garni.json", "w+");        
 fwrite($file, $export);
 fclose($file);
